d0cdbeb35ee09649d05b2d2fb15510a5e00f6e99
WRX <wrxpowered@gmail.com>
2022-01-14 16:17:59 +0800

    add regExp module and url reg exp

---
+++ src/regExp.js
@@ -0,0 +1,5 @@
+/**
+ * reference: 
+ * https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+ */
+export const urlRegExp = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
